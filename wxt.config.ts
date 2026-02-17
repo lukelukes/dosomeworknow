6497ce11f4bf3c1513d3c1d4a29fc3cf27ad66e8
Luke Lukes <126602706+lukelukes@users.noreply.github.com>
2026-02-17 23:09:51 +0200

    chore: add wxt

---
+++ wxt.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "wxt";
+
+export default defineConfig({
+  manifest: {
+    name: "Do Some Work Now",
+    description: "A browser extension to help you focus and do some work.",
+    version: "0.0.1",
+  },
+});
